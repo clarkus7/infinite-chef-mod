7a0e4d871acf8423a850bc8fbc57564433f3505e
clarkus7 <clark.seliger@towerschool.org>
2024-12-06 17:40:33 -0500

    Create bad_stuff.js
    infinite chef bad stuff

---
+++ bad_stuff.js
@@ -0,0 +1,15 @@
+addIngredient("dick",{
+color:"#cd853f", //peru
+innercolor: "ffefd5", //papaya whip
+shape:"curve_thick"
+reactions: {
+    Uhm... Milk: { set1:"happiness" }
+
+addIngredient("skull",{
+  color: "#fffff0", //ivory
+  innercolor: "#fffff0", //ivory
+  shape:"skull"
+
+addIngredient("pain",{
+  color:"#8b0000", //dark red
+  shape:"hexagram_triangle"
